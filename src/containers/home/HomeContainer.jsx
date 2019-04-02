41252f2406d6d96f322f3d7d0517ef87fafef52c
richard naylor <richard.naylor@nike.com>
2019-04-02 08:33:12 -0700

    removed some experimental code and example code leftover from last project. Added relevant functions for map Dispatch to add a user and convert date. Updated mapState to evaluate state from connect to pass into components

--- src/containers/home/HomeContainer.jsx
+++ src/containers/home/HomeContainer.jsx
@@ -2,38 +2,20 @@ import { connect } from 'react-redux'
 import * as actions from '../../modules/users'
 import Home from '../../views/home/Home'
 
-// const mapStateToProps = (state, ownProps) => {
-//     return {
-
-//     }
-// }
-
-
-// const mapStateToProps = ({ counter }) => ({
-//     city1_all: counter.city1,
-//     city2_all: counter.city2,
-//     isIncrementing: counter.isIncrementing,
-//     isDecrementing: counter.isDecrementing,
-// })
-
-const mapStateToProps = () => ({})
-
+const mapStateToProps = (state) => {
+    return {
+        userData: state.users.gitUsers
+    }
+}
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        getUser: (val) => {
-            dispatch(actions.getGitUser(val))
-        },
-        getUserInput: (val) => {
-            dispatch(actions.getUserData(val))
-        },
         addUser: (val) => {
-            console.log('add user container: ', val)
             dispatch(actions.addUser(val))
         },
-        addUser2: (val) => {
-            console.log('add user container: ', val)
-            dispatch(actions.addUser2(val))
+        convertDate: (val) => {
+            const date = new Date(val)
+            return((date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear());
         }
     }
 }
