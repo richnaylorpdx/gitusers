c329a471d17e9851645fd620e793cd5a5e7dd311
richard naylor <richard.naylor@nike.com>
2019-04-02 08:36:56 -0700

    removed some experimental code. Added in ul/li set that iterates through props to produce git user ux

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import PropTypes from "prop-types"
 import { Input } from 'antd'
+import users from '../../modules/users';
 import './Home.css'
 
 export default class Home extends React.Component {
     static propTypes = {
-        getUser: PropTypes.func,
-        getUserInput: PropTypes.func,
         addUser: PropTypes.func,
-        addUser2: PropTypes.func
+        userData: PropTypes.array,
+        convertDate: PropTypes.func,
     }
 
     constructor() {
@@ -19,16 +19,29 @@ export default class Home extends React.Component {
     }
 
      render() {
-        const { getUser, getUserInput, addUser, addUser2 } = this.props
+        const { addUser, userData, convertDate } = this.props
         return (
             <React.Fragment>
                 <Input 
                     onChange={(e) => this.setState({currentValue: e.target.value})}
-                    // onKeyPress={(e) => e.key === 'Enter' && addUser(this.state.currentValue)}
-                    onKeyPress={(e) => e.key === 'Enter' && addUser2(this.state.currentValue)}
-
+                    onKeyPress={(e) => e.key === 'Enter' && addUser(this.state.currentValue)}
                     className='search-box'
                 />
+                <div>
+                    {
+                        userData && userData.map(user => 
+                            <ul key={user.id}>
+                                <li><a href={user.url}>{user.login} | </a></li>
+                                <li>{user.name} | </li>
+                                <li>{user.public_repos} | </li>
+                                <li>{user.public_gists} | </li>
+                                <li>{user.followers} | </li>
+                                <li>{convertDate(user.created_at)}</li>
+                            </ul>
+
+                        )
+                    }
+                </div>
             </React.Fragment>
         )
     }
