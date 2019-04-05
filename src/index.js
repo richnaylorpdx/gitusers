7c5705b1ddb5ad96fd1727ddac90b1543e5ec4f1
richard naylor <richard.naylor@nike.com>
2019-04-05 08:20:35 -0700

    replaced parent div with react fragment, just keeping things tidy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
 import store, { history } from './store'
-import App from './containers/app'
+import App from './app'
 
 import 'sanitize.css/sanitize.css'
 import './index.css'
@@ -13,9 +13,9 @@ const target = document.querySelector('#root')
 render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <div>
+      <React.Fragment>
         <App />
-      </div>
+      </React.Fragment>
     </ConnectedRouter>
   </Provider>,
   target
