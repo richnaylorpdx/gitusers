100a3c2c502f7d44913c87119834ad4ff479b05b
richard naylor <richard.naylor@nike.com>
2019-05-21 08:20:34 -0700

    changed format to 2 spaces

--- src/containers/home/HomeContainer.jsx
+++ src/containers/home/HomeContainer.jsx
@@ -3,31 +3,31 @@ import * as actions from '../../components/users'
 import Home from '../../views/home/Home'
 
 const mapStateToProps = (state) => {
-    return {
-        userInfo: state.users.fetchUsers,
-        success: state.users.success,
-        latestUser: state.users.latestUser
-    }
+  return {
+    userInfo: state.users.fetchUsers,
+    success: state.users.success,
+    latestUser: state.users.latestUser
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        addUser: (val) => {
-            dispatch(actions.addUser(val))
-        },
-        convertDate: (val) => {
-            const date = new Date(val)
-            return((date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear());
-        },
-        getUsers: () => {
-            dispatch(actions.getUsers())
-        },
-    }
+  return {
+    addUser: (val) => {
+      dispatch(actions.addUser(val))
+    },
+    convertDate: (val) => {
+      const date = new Date(val)
+      return ((date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear());
+    },
+    getUsers: () => {
+      dispatch(actions.getUsers())
+    },
+  }
 }
 
 const HomeContainer = connect(
-    mapStateToProps,
-    mapDispatchToProps,
+  mapStateToProps,
+  mapDispatchToProps,
 )(Home)
 
 export default HomeContainer
