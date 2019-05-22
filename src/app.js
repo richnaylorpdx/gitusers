538d1f043b4e30ed1f6f6457f5a2f685740689bc
richard naylor <richard.naylor@nike.com>
2019-05-22 15:26:54 -0700

    added a page header, brought in a style sheet to center it

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
+import { PageHeader } from 'antd';
 import About from './containers/about'
 import HomeContainer from './containers/home/HomeContainer'
+import './app.css'
 
 const App = () => (
-  <div>
-    <header>
+  <React.Fragment>
+    {/* <header>
       <Link to="/">Home</Link>
       <Link to="/about-us">About</Link>
-    </header>
+    </header> */}
+      <PageHeader title="Git Users App" className='header-text' />
+
 
     <main>
-      {/* <Route exact path="/" component={HomeContainer} />
-      <Route exact path="/about-us" component={About} /> */}
+      <Route exact path="/" component={HomeContainer} />
+      <Route exact path="/about-us" component={About} />
     </main>
-  </div>
+  </React.Fragment>
 )
 
 export default App
