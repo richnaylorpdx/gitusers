0f2f127a7e6a73a5e6a52642e8f20d030bb8b8ab
richard naylor <richard.naylor@nike.com>
2019-05-22 09:55:32 -0700

    added ant table. added array of object to format table header and associate table header with appropriate key/val is data set.

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from "prop-types"
-import { Input, Button, Table } from 'antd'
-import users from '../../components/users';
+import { Input, Button, Table, Pagination } from 'antd'
+import users from '../../components/users'
 import './Home.css'
 
 export default class Home extends React.Component {
@@ -27,35 +27,8 @@ export default class Home extends React.Component {
 
   render() {
     const { success, addUser, convertDate, userInfo, latestUser } = this.props
-    const columns = [
-      {
-        title: 'Date',
-        dataIndex: 'date',
-        width: 200,
-      },
-      {
-        title: 'Amount',
-        dataIndex: 'amount',
-        width: 100,
-      },
-      {
-        title: 'Type',
-        dataIndex: 'type',
-        width: 100,
-      },
-      {
-        title: 'Note',
-        dataIndex: 'note',
-        width: 100,
-      },
-      {
-        title: 'Action',
-        key: 'action',
-        render: () => <a href="javascript:;">Delete</a>,
-      },
-    ]
 
-    const columnsFrFb = [
+    const columns = [
       {
         title: 'Username',
         dataIndex: 'login',
@@ -91,32 +64,8 @@ export default class Home extends React.Component {
         dataIndex: 'created_at',
         width: 100,
       },
-
     ]
 
-    const data = [
-      {
-        key: 0,
-        date: '2018-02-11',
-        amount: 120,
-        type: 'income',
-        note: 'transfer',
-      },
-      {
-        key: 1,
-        date: '2018-03-11',
-        amount: 243,
-        type: 'income',
-        note: 'transfer',
-      },
-      {
-        key: 2,
-        date: '2018-04-11',
-        amount: 98,
-        type: 'income',
-        note: 'transfer',
-      },
-    ]
     return (
       <React.Fragment>
         <Input
@@ -130,50 +79,12 @@ export default class Home extends React.Component {
         {
           (!success && latestUser !== null) && <h6>Error adding "{latestUser}" to the db.</h6>
         }
-        <table className='git-users'>
-          <tr className='git-user-table-header'>
-            <th>Username</th>
-            <th>Name</th>
-            <th>Public Repos</th>
-            <th>Public Gists</th>
-            <th>Followers</th>
-            <th>Following</th>
-            <th>Date Created</th>
-          </tr>
-          <tr>
-            <td>cruzies dad</td>
-            <td>cruz dad</td>
-            <td>10</td>
-            <td>20</td>
-            <td>30</td>
-            <td>40</td>
-            <td>04/02/2019</td>
-          </tr>
-          <tr>
-            <td>cruzies dad</td>
-            <td>cruz dad</td>
-            <td>10</td>
-            <td>20</td>
-            <td>30</td>
-            <td>40</td>
-            <td>04/02/2019</td>
-          </tr>
-          {
-            userInfo && userInfo.map(user =>
-              <tr>
-                <td><a href={user.url}>{user.login}</a></td>
-                <td>{user.name}</td>
-                <td>{user.public_repos}</td>
-                <td>{user.public_gists}</td>
-                <td>{user.followers}</td>
-                <td>{user.following}</td>
-                <td>{convertDate(user.created_at)}</td>
-              </tr>
-            )
-          }
-        </table>
-        <Table bordered columns={columns} dataSource={data} />;
-        <Table bordered columns={columnsFrFb} dataSource={userInfo} />;
+        <Table 
+          bordered
+          columns={columns}
+          dataSource={userInfo} 
+          pagination='both'
+        />
       </React.Fragment >
     )
   }
